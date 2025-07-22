75ccd3ea4cbd55cf6d452a5758723e5588ab08b9
Shahria Yesmin sobi <shahriachobi@gmail.com>
2025-07-22 21:22:17 +0600

    Add files via upload

---
+++ hotelmenu.py
@@ -0,0 +1,9 @@
+menu={
+    'Pizza':40,
+    'Pasta':50,
+    'Burger':60,
+    'Salad':70,
+    'Coffee':80,
+}
+print("Welcome to PYTHON Restaurant")
+print("Pizza: Rs40\nPasta: Rs50\nBurger: Rs60\nSalad: Rs70\nCoffee:Rs80 ")
